f6be71e21be20a383cce72d55796845753dfbfd6
Nathen Drees <761005+nadrees@users.noreply.github.com>
2025-06-08 17:02:14 -0500

    Set up bare metal main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
