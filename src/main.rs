524e48d2900733d8ec92022afd5d3ba9d461fd77
Nathen Drees <761005+nadrees@users.noreply.github.com>
2025-06-08 17:59:38 -0500

    Set up booloader api crate

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,28 @@ fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+static HELLO: &[u8] = b"Hello World!";
+
+/// VGA color Cyan
+const CYAN: u8 = 0xb;
+
+fn kernel_main(_boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // writing for VGA. We write the character followed by
+            // a color byte, so each character actually takes up 2
+            // bytes of space.
+
+            // write the character byte to memory.
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // write the byte color
+            *vga_buffer.offset(i as isize * 2 + 1) = CYAN;
+        }
+    }
+
     loop {}
 }
+
+bootloader_api::entry_point!(kernel_main);
